6f89da8bdc0e308e2f3c0349e4588dadfb287a8d
pyutts <wiadyana6@gmail.com>
2025-07-10 22:22:15 +0800

    Redesain

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,6 @@ function App() {
         <Footer />
       </div>
 
-      {/* Modal di atas segalanya */}
       {selectedProject && (
         <ProjectModal project={selectedProject} onClose={closeProjectModal} />
       )}
